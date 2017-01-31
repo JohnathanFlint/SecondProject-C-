b1557dad707238088f79a2603e151aaab65db1af
JohnathanFlint <nicholas.white142@gmail.com>
2017-01-31 09:02:18 -0700

    Initial Commit

---
+++ SecondProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SecondProject
+//
+//  Created by White, Nicholas on 1/31/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
